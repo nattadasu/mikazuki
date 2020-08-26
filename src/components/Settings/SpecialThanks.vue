be01c1d339aafb49fc14553e30b3ff1547c531a3
NicoAiko <n.linnemann@nicoaiko.de>
2020-08-26 21:03:31 +0000

    Satisfy prettier 4real now

--- src/components/Settings/SpecialThanks.vue
+++ src/components/Settings/SpecialThanks.vue
@@ -3,15 +3,14 @@
     <h1 class="pb-1">{{ $t('menus.settings.specialThanks') }}</h1>
 
     <v-row>
-      <v-col
-        cols="12"
-        md="6"
-        class="py-1"
-        v-for="(item, index) in translators"
-        :key="`translator_${index}`"
-      >
+      <v-col cols="12" md="6" class="py-1" v-for="(item, index) in translators" :key="`translator_${index}`">
         <h4 class="py-1">
-          <v-icon v-for="(icon, i) in item.icons" :key="`translator_${index}_icon_${i}`" left :class="`flag-icon flag-icon-${icon}`" />
+          <v-icon
+            v-for="(icon, i) in item.icons"
+            :key="`translator_${index}_icon_${i}`"
+            left
+            :class="`flag-icon flag-icon-${icon}`"
+          />
           <span class="title">{{ item.name }}</span>
         </h4>
         <div class="body-2">{{ item.message }}</div>
