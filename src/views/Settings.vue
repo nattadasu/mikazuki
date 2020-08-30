298d02121931334065f32a2ba84eae25b974a52e
NicoAiko <n.linnemann@nicoaiko.de>
2020-08-30 12:46:27 +0200

    Remove old special thanks and changelog files as well as old settings views

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -8,15 +8,7 @@
           </v-tab>
 
           <v-tabs-items v-model="tabs">
-            <AppSettings :tab-key="appSettingsTab.key" />
-
             <AniList :tab-key="aniListTab.key" />
-
-            <About :tab-key="aboutTab.key" />
-
-            <Changelog :tab-key="changelogTab.key" />
-
-            <SpecialThanks :tab-key="specialThanksTab.key" />
           </v-tabs-items>
         </v-tabs>
       </v-card-text>
@@ -27,11 +19,7 @@
 <script lang="ts">
 import { TranslateResult } from 'vue-i18n';
 import { Component, Vue } from 'vue-property-decorator';
-import AppSettings from './Settings/AppSettings.vue';
 import AniList from './Settings/AniList.vue';
-import About from './Settings/About.vue';
-import Changelog from './Settings/Changelog.vue';
-import SpecialThanks from './Settings/SpecialThanks.vue';
 
 interface ISettingTab {
   key: string;
@@ -41,11 +29,7 @@ interface ISettingTab {
 
 @Component({
   components: {
-    About,
     AniList,
-    AppSettings,
-    Changelog,
-    SpecialThanks,
   },
 })
 export default class Settings extends Vue {
@@ -56,41 +40,13 @@ export default class Settings extends Vue {
    * @returns {ISettingTab[]} the Settings tabs
    */
   get settingsTabs(): ISettingTab[] {
-    return [this.appSettingsTab, this.aniListTab, this.aboutTab, this.changelogTab, this.specialThanksTab];
-  }
-
-  get appSettingsTab(): ISettingTab {
-    return {
-      key: 'appSettings',
-      value: this.$t('menus.settings.appSettings'),
-    };
+    return [this.aniListTab];
   }
 
   get aniListTab(): ISettingTab {
     return {
       key: 'aniList',
       value: this.$t('menus.settings.aniList'),
-    };
-  }
-
-  get aboutTab(): ISettingTab {
-    return {
-      key: 'about',
-      value: this.$t('menus.settings.about'),
-    };
-  }
-
-  get changelogTab(): ISettingTab {
-    return {
-      key: 'changelog',
-      value: this.$t('menus.settings.changelog'),
-    };
-  }
-
-  get specialThanksTab(): ISettingTab {
-    return {
-      key: 'specialThanks',
-      value: this.$t('menus.settings.specialThanks'),
     };
   }
 }
